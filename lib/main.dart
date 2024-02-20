5f19ac52fada5022a09a211e0e43fa0a62464e5c
Omar Gaber <omaargaabeer@gmail.com>
2024-02-20 21:06:59 +0200

    Shared preference for language

--- lib/main.dart
+++ lib/main.dart
@@ -13,21 +13,28 @@ import 'package:shared_preferences/shared_preferences.dart';
 Future<void> main()  async {
   WidgetsFlutterBinding.ensureInitialized();
   late bool dark  ;
+  late String oldLanguage ;
   SharedPreferences preferences = await SharedPreferences.getInstance();
   if(!preferences.containsKey("is dark")){
     print("false");
     preferences.setBool("is dark", false);
     dark = false ;
   }else{
-     dark = !preferences.getBool("is dark")!;
-     print("dark : $dark");
+    dark = !preferences.getBool("is dark")!;
+    print("dark : $dark");
 
   }
 
-  // String? oldLanguage = preferences.get("language") as String?;
+  if(!preferences.containsKey("language")){
+    preferences.setString("language", "ar");
+  }else{
+      oldLanguage = (preferences.get("language") as String?)!;
+
+  }
+
   runApp( ChangeNotifierProvider(
-      create: await(context)=> SettingsProvider()..changeAppTheme(dark!),
-        // ..changeAppLanguage(oldLanguage!),
+      create: await(context)=> SettingsProvider()..changeAppTheme(dark!)
+      ..changeAppLanguage(oldLanguage!),
       child: const IslamyApp()));
 
 }
--- lib/test.dart
+++ lib/test.dart
@@ -1,30 +1,26 @@
-import 'dart:io';
-
-void main() {
-  List<String> titles= [];
-  List<String> content= [];
-  try {
-
-    File file = File('D:\\route\\islamy\\assets\\texts\\ahadeth.txt');
-    String contents = file.readAsStringSync();
-    List<String> ahadethContent = contents.trim().split("#");
-
-
-    for(int i =0 ; i<ahadethContent.length ; i++){
-      List<String> list2 = ahadethContent[i].toString().trim().split("\n");
-      titles.add(list2[0]);
-      content.add(list2[1]);
-      list2.removeAt(0);
-      print(titles[i]);
-      // print(content[i]);
-      print("\n");
-
-    }
-
-
-
-
-  } catch (e) {
-    print('Error reading the file: $e');
-  }
-}
+// import 'dart:convert';
+// import 'package:flutter/material.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:audioplayers/audioplayers.dart';
+//
+//
+// class Test extends StatelessWidget {
+//   final player = AudioPlayer();
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: Colors.white,
+//       child: Scaffold(
+//         body: Center(
+//           child: Text("getPost()"),
+//         ),
+//       ),
+//     );
+//   }
+//
+// play() async {
+//   await player.play(UrlSource('https://mp3quran.net/api/v3/radios'));
+//
+// }
+//
+// }
